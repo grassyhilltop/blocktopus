f6a6a3ddb9a576641149a32503b58cce3326b859
Kevin Durfee <kdurfee@dn0a2273a4.sunet>
2015-04-21 18:53:25 -0700

    changed the default backspace keypress handler to prevent going back in browser

--- octoWebExpress/public/javascripts/clientApp.js
+++ octoWebExpress/public/javascripts/clientApp.js
@@ -342,7 +342,36 @@ function ClientApp() {
 	
 	this.setupCodeBlocks = function () {
 		window.addEventListener("click", this.masterClickHandler); 
-		window.addEventListener("dblclick", this.masterDoubleClickHandler); 
+		window.addEventListener("dblclick", this.masterDoubleClickHandler);
+		
+		//Prevents backspace except in the case of textareas and text inputs to prevent user navigation.
+		$(document).keydown(function (e) {
+			var preventKeyPress;
+			if (e.keyCode == 8) {
+				var d = e.srcElement || e.target;
+				switch (d.tagName.toUpperCase()) {
+					case 'TEXTAREA':
+						preventKeyPress = d.readOnly || d.disabled;
+						break;
+					case 'INPUT':
+						preventKeyPress = d.readOnly || d.disabled ||
+							(d.attributes["type"] && $.inArray(d.attributes["type"].value.toLowerCase(), ["radio", "checkbox", "submit", "button"]) >= 0);
+						break;
+					case 'DIV':
+						preventKeyPress = d.readOnly || d.disabled || !(d.attributes["contentEditable"] && d.attributes["contentEditable"].value == "true");
+						break;
+					default:
+						preventKeyPress = true;
+						break;
+				}
+			}
+			else
+				preventKeyPress = false;
+
+			if (preventKeyPress)
+				e.preventDefault();
+		});
+		//Prevents backspace except in the case of textareas and text inputs to prevent user navigation.
 	};
 	
 	// Functions to call when the app is first opened
