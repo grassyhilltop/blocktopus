43f46d5c8b79296930b919bbb9bee28b816f1017
Charles Guan <charlesguan94@gmail.com>
2016-04-01 11:27:19 -0700

    Resolve merge conflict

--- firmware/modules/output.c
+++ firmware/modules/output.c
@@ -1,7 +1,11 @@
+/* Implements an output module.
+ * Acts as a PWM to enable both analog and digital output.
+ */
 #include "output.h"
 #include "usbdrv/usbdrv.h"
 #include "main.h"
 #include "hardware.h"
+#include <stdbool.h>
 
 static unsigned int duty_cycle = 0;
 static const unsigned int iterations_p_cycle = 100;
@@ -11,37 +15,39 @@ void init_output(void){
 }
 
 void output_main_loop(void){
-	static int status = 0;
+	static bool status = false;
 	unsigned int counter = 0;
 	
 	for (counter = 0; counter < iterations_p_cycle ; counter ++){
+		/* note from charles: counter is incremented twice per loop
+		 * the increment is presumably intentional, but not sure */
 		if ((counter++ % iterations_p_cycle) < duty_cycle){
 			if (!status){
 				turn_on();
-				status = 1;
+				status = true;
 			}
 		}
 		else{
 			if(status){
 				turn_off();
-				status = 0;
+				status = false;
 			}
 		}
 	}
 }
 
 void output_usb_input_handler(unsigned char * midiMsg, unsigned char len){
-
-		if(midiMsg[1] == 0x90){
-			// blink();
-			// Turn on OUTPUT
-			duty_cycle = iterations_p_cycle;			
-		}
-		else if (midiMsg[1] == 0xE3){
-			duty_cycle = (unsigned int) midiMsg[3];
-		}
-		// Note off message 0x80
-		else if(midiMsg[1] == 0x80 ){				
-			duty_cycle = 0;
-		} 			
+	// TODO if len is not used, don't pass it in
+	if(midiMsg[1] == 0x90){
+		// blink();
+		// Turn on OUTPUT
+		duty_cycle = iterations_p_cycle;
+	}
+	else if (midiMsg[1] == 0xE3){
+		duty_cycle = (unsigned int) midiMsg[3];
+	}
+	// Note off message 0x80
+	else if(midiMsg[1] == 0x80 ){
+		duty_cycle = 0;
+	}
 }
