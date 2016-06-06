7f1fb49302972902eab900c2f7a0d4bfb0fe2b70
Charles Guan <charlesguan94@gmail.com>
2016-06-06 14:46:52 -0700

    updating deprecated defines

--- firmware/config.h
+++ firmware/config.h
@@ -1,26 +1,10 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-/* TODO: for some reason using enums results in code auto-generation or parsing errors. */
-/* typedef enum ModuleType { */
-/* 	ANALOG_INPUT, */
-/* 	ANALOG_OUTPUT, */
-/* 	DIGITAL_INPUT, */
-/* 	DIGITAL_OUTPUT, */
-/* 	I2C_DEVICE, */
-/* /1* Left-over old module types. *1/ */
-/*   KNOB, */
-/*   BUTTON, */
-/*   OUTPUT, */
-/*   RGB_LED, */
-/*   COMPASS, */
-/*   ACCELEROMETER, */
-/* } ModuleType; */
-
 // MODULE_TYPE is passed in via Makefile flag
-#define KNOB   1
-#define BUTTON 2
-#define OUTPUT 3
+#define KNOB   1 // Deprecated, use ANALOG_INPUT
+#define BUTTON 2 // Deprecated, use DIGITAL_INPUT
+#define OUTPUT 3 // Deprecated, use ANALOG_OUTPUT or DIGITAL_OUTPUT
 #define RGB_LED 4
 #define COMPASS 5
 #define ACCELEROMETER 6
@@ -28,8 +12,11 @@
 #define ANALOG_OUTPUT 8
 #define DIGITAL_INPUT 9
 #define DIGITAL_OUTPUT 10
-#define I2C_DEVICE 11
+#define I2C_DEVICE 11 // Not implemented fully
 
+// SysEx message defines. Used in lib/midi.c
+// Define values must be representable by a byte
+#define WDT_RESET 101
 #define REQUEST_DEVICE_TYPE 99
 
 #endif
